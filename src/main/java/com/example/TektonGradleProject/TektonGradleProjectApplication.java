3b5f9c1f9b1dc44aa918005027cb21671c685b8b
Gerrits Marc <gerrits.marc@hotmail.com>
2022-08-30 21:15:05 +0200

    first upload

---
+++ src/main/java/com/example/TektonGradleProject/TektonGradleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.TektonGradleProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TektonGradleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TektonGradleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TektonGradleProject/TektonGradleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TektonGradleProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TektonGradleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
